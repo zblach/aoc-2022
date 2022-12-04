c8888924d55b8a7f9ba6f97bc826c5a7c03de792
Zak Blacher <zblach@The-Cloud.local>
2022-12-04 10:04:29 -0800

    some generics experimentation

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,14 +1,16 @@
+use std::cmp::{max, min};
+
 use scanf::sscanf;
 
-struct pair(i32, i32);
+struct Pair<T>(T, T);
 
-impl pair {
-    fn contains(&self, other: &pair) -> bool {
+impl<T: std::cmp::Ord> Pair<T> {
+    fn contains(&self, other: &Pair<T>) -> bool {
         self.0 <= other.0 && self.1 >= other.1
     }
 
-    fn overlaps(&self, other: &pair) -> bool {
-        self.0 <= other.0 && other.0 <= self.1
+    fn overlaps(&self, other: &Pair<T>) -> bool {
+        max(&self.0, &other.0) <= min(&self.1, &other.1)
     }
 }
 
@@ -19,7 +21,7 @@ pub fn part_one(input: &str) -> Option<u32> {
         let (mut a, mut b, mut c, mut d) = (0, 0, 0, 0);
         sscanf!(line, "{}-{},{}-{}", a, b, c, d).unwrap();
 
-        let (r1, r2) = (pair(a, b), pair(c, d));
+        let (r1, r2) = (Pair(a, b), Pair(c, d));
 
         if r1.contains(&r2) || r2.contains(&r1) {
             overlap_count += 1;
@@ -35,9 +37,9 @@ pub fn part_two(input: &str) -> Option<u32> {
         let (mut a, mut b, mut c, mut d) = (0, 0, 0, 0);
         sscanf!(line, "{}-{},{}-{}", a, b, c, d).unwrap();
 
-        let (r1, r2) = (pair(a, b), pair(c, d));
+        let (r1, r2) = (Pair(a, b), Pair(c, d));
 
-        if r1.overlaps(&r2) || r2.overlaps(&r1) {
+        if r1.overlaps(&r2) {
             overlap_count += 1;
         }
     }
